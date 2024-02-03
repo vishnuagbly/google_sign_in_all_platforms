8fc8379ee40670fd6e49c81259b16bb91a59a657
Vishnu Agarwal <vishnuagbly@gmail.com>
2024-02-04 00:48:39 +0530

    Added a new package called "google_sign_in_all_desktop"

--- google_sign_in_all_platforms_windows/lib/google_sign_in_all_platforms_windows.dart
+++
@@ -1,6 +0,0 @@
-import 'package:google_sign_in_all_platforms_macos/google_sign_in_all_platforms_macos.dart';
-
-export 'package:google_sign_in_all_platforms_macos/google_sign_in_all_platforms_macos.dart';
-
-///This is basically typedef of [GoogleSignInAllPlatformsMacOS]
-typedef GoogleSignInAllPlatformsWindows = GoogleSignInAllPlatformsMacOS;
