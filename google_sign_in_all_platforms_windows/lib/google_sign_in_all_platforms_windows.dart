d6c3a4bec9cf6d67170b1c2927cbbcfd2ccd7fbd
Vishnu Agarwal <vishnuagbly@gmail.com>
2024-02-03 20:54:20 +0530

    Removed windows folder from MacOS Plugin. Added Windows Plugin

---
+++ google_sign_in_all_platforms_windows/lib/google_sign_in_all_platforms_windows.dart
@@ -0,0 +1,6 @@
+import 'package:google_sign_in_all_platforms_macos/google_sign_in_all_platforms_macos.dart';
+
+export 'package:google_sign_in_all_platforms_macos/google_sign_in_all_platforms_macos.dart';
+
+///This is basically typedef of [GoogleSignInAllPlatformsMacOS]
+typedef GoogleSignInAllPlatformsWindows = GoogleSignInAllPlatformsMacOS;
