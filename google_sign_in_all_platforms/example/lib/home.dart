94eb60b3c40099376f9e0f0123c80b0c005ca731
Vishnu Agarwal <vishnuagbly@gmail.com>
2024-02-06 01:03:11 +0530

    [Main] updated example app

--- google_sign_in_all_platforms/example/lib/home.dart
+++ google_sign_in_all_platforms/example/lib/home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_sign_in_all_platforms/google_sign_in_all_platforms.dart';
-import 'package:helpful_components/helpful_components.dart';
+import 'package:google_sign_in_all_platforms_example/secrets.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,69 +10,34 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  static const _kClientId =
-      '1068367409273-7hqioe7d0j1tcokk4uk9satmfvcpj1bj.apps.'
-      'googleusercontent.com';
-
-  static const _kClientSecret = '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';
-
-  late final GoogleSignIn _googleSignIn;
-
-  @override
-  void initState() {
-    _googleSignIn = GoogleSignIn(
-      params: const GoogleSignInParams(
-        clientId: _kClientId,
-        clientSecret: _kClientSecret,
-        redirectPort: 3000,
-      ),
-    );
-    super.initState();
-  }
+  var _signedIn = false;
+  final GoogleSignIn _googleSignIn = GoogleSignIn(
+    params: const GoogleSignInParams(
+      clientId: kGoogleClientId,
+      clientSecret: kGoogleClientSecret,
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Testing Application')),
+      appBar: AppBar(title: const Text('Desktop Test')),
       body: Center(
-        child: Column(
-          children: [
-            const Spacer(),
-            ElevatedButton(
-              onPressed: () {
-                showDialog<void>(
-                  context: context,
-                  builder: (_) => FutureDialog(
-                    future: _googleSignIn.signInOnline(),
-                    onData: (creds) {
-                      if (creds == null) {
-                        return const CommonAlertDialog(
-                          'Cannot Sign in',
-                          error: true,
-                        );
-                      }
-                      debugPrint('${creds.toJson()}');
-                      return const CommonAlertDialog('Successfully Signed in');
-                    },
-                  ),
-                );
-              },
-              child: const Text('Sign In'),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                showDialog<void>(
-                  context: context,
-                  builder: (_) => FutureDialog(
-                    future: _googleSignIn.signOut(),
-                  ),
-                );
-              },
-              child: const Text('Sign Out'),
-            ),
-            const Spacer(),
-          ],
+        child: ElevatedButton(
+          onPressed: () async {
+            GoogleSignInCredentials? creds;
+            if (!_signedIn) {
+              creds = await _googleSignIn.signInOffline();
+              creds ??= await _googleSignIn.signInOnline();
+            } else {
+              await _googleSignIn.signOut();
+            }
+            final signedIn = creds != null;
+            setState(() {
+              _signedIn = signedIn;
+            });
+          },
+          child: Text(_signedIn ? 'Sign Out' : 'Sign in'),
         ),
       ),
     );
---
+++ google_sign_in_all_platforms/example/lib/secrets.dart
@@ -0,0 +1,9 @@
+const kGoogleSignInScopes = [
+  'https://www.googleapis.com/auth/drive',
+  'https://www.googleapis.com/auth/userinfo.profile',
+  'https://www.googleapis.com/auth/userinfo.email',
+];
+const String kGoogleClientId =
+    '944102191851-bnpdpdmmdndkaehmm0f8oidgvhsbqe6n.apps.googleusercontent.com';
+const String kGoogleClientSecret = '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';
+const int kGoogleRedirectUriPort = 8000;
