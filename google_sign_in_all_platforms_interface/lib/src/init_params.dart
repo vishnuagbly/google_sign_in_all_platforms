ee81c614a321ceb06c07e6071ed318262d8db1fe
Vishnu Agarwal <vishnuagbly@gmail.com>
2024-02-08 02:03:27 +0530

    [Interface] Removed "drive" scope from default values of GoogleSignInParams.scopes

--- google_sign_in_all_platforms_interface/lib/src/init_params.dart
+++ google_sign_in_all_platforms_interface/lib/src/init_params.dart
@@ -13,7 +13,6 @@ class GoogleSignInParams {
     this.retrieveAccessToken = _defaultRetrieveAccessToken,
     this.deleteAccessToken = _defaultDeleteAccessToken,
     this.scopes = const [
-      'https://www.googleapis.com/auth/drive',
       'https://www.googleapis.com/auth/userinfo.profile',
       'https://www.googleapis.com/auth/userinfo.email',
     ],
