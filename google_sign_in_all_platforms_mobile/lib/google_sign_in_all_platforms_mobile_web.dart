22781995a70fec626648a063b575e486b8d971b3
DataBPaidiatriko <databpaidiatriko@gmail.com>
2024-07-05 14:22:52 +0300

    Create google_sign_in_all_platforms_mobile_web.dart

---
+++ google_sign_in_all_platforms_mobile/lib/google_sign_in_all_platforms_mobile_web.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+import 'google_sign_in_all_platforms_mobile.dart';
+
+class GoogleSignInAllPlatformsMobileWeb {
+  static void registerWith(Registrar registrar) {
+    GoogleSignInAllPlatformsMobile.registerWith();
+  }
+}
