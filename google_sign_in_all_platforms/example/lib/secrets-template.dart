7f4a3460f484dd61cd3fcd66520161cca6d5382f
Vishnu Agarwal <vishnuagbly@gmail.com>
2024-12-13 18:48:21 +0530

    Added secrets-template.dart file

---
+++ google_sign_in_all_platforms/example/lib/secrets-template.dart
@@ -0,0 +1,8 @@
+const kGoogleSignInScopes = [
+  'https://www.googleapis.com/auth/userinfo.profile',
+  'https://www.googleapis.com/auth/userinfo.email',
+];
+const String kGoogleClientId =
+    '292705536169-t2u0r4rl1124bsbca3iuklkobb7lnbf4.apps.googleusercontent.com';
+const String kGoogleClientSecret = 'GOCOPA-jn_BCKmFtsOhMhW-ivSfbeKVYjTp';
+const int kGoogleRedirectUriPort = 8000;
