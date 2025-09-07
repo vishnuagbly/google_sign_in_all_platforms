7927d9f3c63bd2a9b980b81e94f7426615ed9c12
Vishnu Agarwal <vishnuagbly@gmail.com>
2025-09-08 03:20:51 +0530

    Refactor platform detection: To get WASM Compatibility check.

--- google_sign_in_all_platforms_interface/lib/src/extensions/platform.dart
+++ google_sign_in_all_platforms_interface/lib/src/extensions/platform.dart
@@ -1,8 +1,3 @@
-import 'dart:io';
-
-///Whether the current [Platform] is a desktop or not.
-bool get isDesktop =>
-    Platform.isMacOS || Platform.isLinux || Platform.isWindows;
-
-///Whether the current [Platform] is a mobile or not.
-bool get isMobile => Platform.isIOS || Platform.isWindows;
+export 'platform_stub.dart'
+    if (dart.library.html) 'platform_web.dart'
+    if (dart.library.io) 'platform_io.dart';
---
+++ google_sign_in_all_platforms_interface/lib/src/extensions/platform_stub.dart
@@ -0,0 +1,14 @@
+/// Whether the app is running on the web.
+bool get isWeb => false;
+
+/// Whether the app is running on a desktop platform.
+bool get isDesktop => false;
+
+/// Whether the app is running on a mobile platform.
+bool get isMobile => false;
+
+/// Whether the web app is running on a desktop platform.
+bool get isDesktopWeb => false;
+
+/// Whether the web app is running on a mobile platform.
+bool get isMobileWeb => false;
