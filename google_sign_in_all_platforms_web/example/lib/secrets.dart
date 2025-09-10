44db57d83ac63fdb7c7917a8aff5170bef2409e0
Vishnu Agarwal <vishnuagbly@gmail.com>
2025-09-11 01:43:04 +0530

    deleted secrets file

--- google_sign_in_all_platforms_web/example/lib/secrets.dart
+++
@@ -1,7 +0,0 @@
-const kGoogleSignInScopes = [
-  'https://www.googleapis.com/auth/userinfo.profile',
-  'https://www.googleapis.com/auth/userinfo.email',
-];
-const String kGoogleClientId =
-    '292705536169-t2u0r4rl1457bsbca3iuklkobb7lnbf4.apps.googleusercontent.com';
-const int kGoogleRedirectUriPort = 8000;
